e8f60bf1a3e733e315583186953677577c05f6a4
joelabl <82639814+joelabl@users.noreply.github.com>
2025-04-07 18:43:46 +0200

    Add an example script for downloading the data

---
+++ examples/scripts/scrape_data.py
@@ -0,0 +1,8 @@
+"""This script downloads some pixar datasets."""
+
+from pixarfilms.utils import download_pixar_datasets
+
+
+if __name__ == "__main__":
+    download_pixar_datasets()
+    print("Pixar datasets downloaded successfully.")
